22e2c1a6c3df81873c6168a9f41d3cde32e87b91
blizzard <blizzard-bo.wu@cn.abb.com>
2023-12-07 13:53:23 +0800

    update main.c

--- src/main.c
+++ src/main.c
@@ -14,11 +14,17 @@ int main(void)
     int openssl_patch = (openssl_version >> 12) & 0xFF;
     int openssl_status = openssl_version & 0xFF;
 
-    LOG_DEBUG("OpenSSL version: %d.%d.%d%c\n", openssl_major, openssl_minor, openssl_patch, openssl_status);
+    while(1)
+    {
+        LOG_DEBUG("OpenSSL version: %d.%d.%d%c\n", openssl_major, openssl_minor, openssl_patch, openssl_status);
 
-	LOG_INFO("ABB Blizzard!\n");
-	
-	return 0;
+        LOG_INFO("ABB Blizzard!\n");
+
+        sleep(2);
+    }
+
+        return 0;
+
 }
 
 
